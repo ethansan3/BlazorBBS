bcf4dcf8a22cff5e2a205403cf71fb875f5eb045
Iida Hiroki <minoru113@hotmail.com>
2019-07-14 13:10:14 +0900

    commit

---
+++ BlazorBBS/Data/ThreadContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BlazorBBS.Data
+{
+	public class ThreadContext : IdentityDbContext
+	{
+		public ThreadContext(DbContextOptions<ThreadContext> option) : base(option)
+		{
+		}
+
+		public DbSet<ThreadData> Thread { get; set; }
+	}
+
+	public class ThreadData
+	{
+		public int Id { get; set; }
+
+		public string Email { get; set; }
+		public string Name { get; set; }
+		public string Title { get; set; }
+		public string Text { get; set; }
+		public DateTime CreateDateTime { get; set; }
+		public DateTime LastUpdate { get; set; }
+
+		[NotMapped]
+		public List<ThreadResponseData> Response { get; set; } = new List<ThreadResponseData>();
+
+	}	
+}
---
+++ BlazorBBS/Data/ThreadResponseContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace BlazorBBS.Data
+{
+	public class ThreadResponseContext : IdentityDbContext
+	{
+		public ThreadResponseContext(DbContextOptions<ThreadResponseContext> option) : base(option)
+		{
+		}
+		public DbSet<ThreadResponseData> ThreadResponse { get; set; }
+	}
+
+	public class ThreadResponseData
+	{
+		public int Id { get; set; }
+
+		public int ResponseId { get; set; }
+
+		public string Email { get; set; }
+		public string Name { get; set; }
+		public string Text { get; set; }
+		public DateTime CreateDateTime { get; set; }
+		public DateTime LastUpdate { get; set; }
+
+		public int ParentThreadId { get; set; }
+
+		[NotMapped]
+		public ThreadData ParentThread { get; set; }
+	}
+}
